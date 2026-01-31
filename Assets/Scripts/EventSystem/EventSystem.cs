6ec87469b6f7a385985de3f8b9c871aa4803d88d
Draginalo <tropicaltisa@gmail.com>
2026-01-31 11:53:26 -0500

    Finished game loop first pass.

--- Assets/Scripts/EventSystem/EventSystem.cs
+++ Assets/Scripts/EventSystem/EventSystem.cs
@@ -14,4 +14,13 @@ public static class EventSystem
 
     public static UnityAction<int> OnAddSkiballPoints;
     public static void AddSkiBallPoints(int pointsToAdd) { OnAddSkiballPoints?.Invoke(pointsToAdd); }
+
+    public static UnityAction<NPCDialogueData, int> OnStartNextOrder;
+    public static void StartNextOrder(NPCDialogueData customerData, int orderIndex) { OnStartNextOrder?.Invoke(customerData, orderIndex); }
+
+    public static UnityAction<OrderQuality> OnRateOrder;
+    public static void RateOrder(OrderQuality quality) { OnRateOrder?.Invoke(quality); }
+
+    public static UnityAction<bool> OnRateShot;
+    public static void RateShot(bool goodShot) { OnRateShot?.Invoke(goodShot); }
 }
